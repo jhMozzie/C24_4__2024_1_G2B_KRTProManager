d8ee95c8b6b1d34be766919c85ca6d4b06c29e2d
Joseph Flores <jhmozzie@gmail.com>
2024-06-28 16:24:48 -0500

    Backend Completed !

--- krtpromanager-SpringBoot/src/main/java/com/krtpromanager/krtpromanagerSpringBoot/dto/UsuarioDto.java
+++ krtpromanager-SpringBoot/src/main/java/com/krtpromanager/krtpromanagerSpringBoot/dto/UsuarioDto.java
@@ -2,7 +2,6 @@ package com.krtpromanager.krtpromanagerSpringBoot.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
-import java.util.List;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
